94b8a52b1045623e40244eceadad0952b173b49e
Steven Gonzalez <uniacid@gmail.com>
2015-04-28 02:46:31 -0400

    Updating accounts and api

--- accounts.js
+++ accounts.js
@@ -2,21 +2,26 @@ var express = require('express'),
 	bodyParser = require('body-parser'),
 	session	   = require('express-session'),
 	Bourne     = require('bourne'),
-	crypto     = require('crypto');
+	crypto     = require('crypto'),
+	path       = require('path');
 
 var router = express.Router(),
-	db = new Bourne('user.json');
+	db = new Bourne('users.json');
 
 function hash (password) {
 	return crypto.createHash('sha256').update(password).digest('hex');
 }
 
 router
-	.use(bodyParser.urlencoded())
+	.use(bodyParser.urlencoded({ extended: true }))
 	.use(bodyParser.json())
-	.use(session({ secret: 'dsafsadfsdaflsadfkdsja32432lfk23' }))
+	.use(session({
+		secret: 'magic cat',
+		resave: false,
+		saveUninitialized: true
+	}))
 	.get('/login', function (req, res) {
-		res.sendfile('public/login.html');
+		res.sendFile(path.join(__dirname, '/public', 'login.html'));
 	})
 	.post('/login', function (req, res) {
 		var user = {
@@ -32,5 +37,35 @@ router
 			}
 		});
 	})
-	.post('/create')
-	.get('/logout')
+	.post('/register', function (req, res) {
+		var user = {
+			username: req.body.username,
+			password: hash(req.body.password),
+			options: {}
+		};
+
+		db.find({ username: user.username }, function(err, data) {
+			if (!data.length) {
+				db.insert(user, function (err, data) {
+					req.session.userId = data.id;
+					req.redirect('/');
+				});
+			} else {
+				res.redirect('/login');
+			}
+		});
+	})
+	.get('/logout', function (req, res) {
+		req.session.userId = null;
+		req.redirect('/');
+	})
+	.use(function (req, res, next) {
+		if (req.session.userId) {
+			db.findOne({ id: req.session.userId }, function (err, data) {
+				req.user = data;
+			});
+		}
+		next();
+	});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 var express = require('express'),
     api     = require('./api'),
-    app     = express();
+    users   = require('./accounts'),
+    app     = express(),
+    path 	= require('path');
 
 app
     .use(express.static('./public'))
+    .use(users)
     .use('/api', api)
     .get('*', function (req, res) {
-        res.sendfile('public/main.html');
+    	if (!req.user) {
+    		res.redirect('/login');
+    	} else {
+        	res.sendFile(path.join(__dirname, '/public', 'main.html'));
+    	}
     })
     .listen(3000);
