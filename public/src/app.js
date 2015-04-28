7c2205e66117c6134158bf3b94009f7310731d85
Steven Gonzalez <uniacid@gmail.com>
2015-04-28 01:45:23 -0400

    Adding public resource files

---
+++ public/src/app.js
@@ -0,0 +1,18 @@
+angular.module('ContactsApp', ['ngRoute', 'ngResource', 'ngMessages'])
+    .config(function($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/contacts', {
+                controller: 'ListController',
+                templateUrl: 'views/list.html'
+            })
+            .when('/contact/new', {
+                controller: 'NewController',
+                templateUrl: 'views/new.html'
+            })
+            .when('/contact/:id', {
+                controller: 'SingleController',
+                templateUrl: 'views/single.html'
+            });
+
+        $locationProvider.html5Mode(true);
+    });
---
+++ public/src/controllers.js
@@ -0,0 +1,48 @@
+angular.module('ContactsApp')
+    .controller('ListController', function($scope, $rootScope, Contact, $location) {
+        $rootScope.PAGE = "all";
+        $scope.contacts = Contact.query();
+        $scope.fields = ['firstName', 'lastName'];
+
+        $scope.sort = function (field) {
+            $scope.sort.field = field;
+            $scope.sort.order = !$scope.sort.order;
+        };
+
+        $scope.sort.field = 'firstName';
+        $scope.sort.order = false;
+
+        $scope.show = function(id) {
+            $location.url('/contact/' + id);
+        }
+    })
+    .controller('NewController', function($scope, $rootScope, Contact, $location) {
+        $rootScope.PAGE = "new";
+        $scope.contact = new Contact({
+            firstName: ['', 'text'],
+            lastName: ['', 'text'],
+            email: ['', 'email'],
+            homePhone: ['', 'tel'],
+            cellPhone: ['', 'tel'],
+            birthday: ['', 'date'],
+            website: ['', 'url'],
+            address: ['', 'text']
+        });
+
+        $scope.save = function() {
+            if ($scope.newContact.$invalid) {
+                $scope.$broadcast('record:invalid');
+            } else {
+                $scope.contact.$save();
+                $location.url('/contacts');
+            }
+        };
+    })
+    .controller('SingleController', function($scope, $rootScope, $location, Contact, $routeParams) {
+        $rootScope.PAGE = "single";
+        $scope.contact = Contact.get({ id: parseInt($routeParams.id, 10) });
+        $scope.delete = function() {
+            $scope.contact.$delete();
+            $location.url('/contacts');
+        };
+    });
